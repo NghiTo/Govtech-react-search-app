26c9b7a32b61cca811eb827b208cfb5bbbb63bdf
Kenzy <tobanghi@gmail.com>
2025-03-24 16:09:23 +0700

    fix: header

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,25 +1,25 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Header from "./Header";
-import { describe, test, expect } from "vitest";
+import { describe, test, expect, beforeEach } from "vitest";
 
 describe("Header Component", () => {
-  test("renders Singapore logo", () => {
+  beforeEach(() => {
     render(<Header />);
-    const logo = screen.getByAltText("Singapore logo");
+  });
+
+  test("renders Singapore logo", () => {
+    const logo = screen.getByAltText("Logo of the Singapore Government");
     expect(logo).toBeInTheDocument();
     expect(logo).toHaveAttribute("src", "/src/assets/R-removebg-preview.png");
   });
 
   test("renders government website text", () => {
-    render(<Header />);
-    
-    expect(screen.getByText("An Official Website of the")).toBeInTheDocument();
+    expect(screen.getByText(/An Official Website of the/i)).toBeInTheDocument();
     expect(screen.getByText("Singapore Government")).toBeInTheDocument();
   });
 
   test("Singapore Government text is bold", () => {
-    render(<Header />);
     const sgText = screen.getByText("Singapore Government");
     expect(sgText).toHaveClass("font-semibold");
   });
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,17 @@
-import lionLogo from "/src/assets/R-removebg-preview.png";
+import lionLogo from "../../assets/R-removebg-preview.png";
 
 const Header = () => {
   return (
     <div className="bg-[#f0f0f0] py-1 px-6 sm:px-12 md:px-20 lg:px-40 flex flex-row gap-4 text-xs">
-      <img src={lionLogo} alt="Singapore logo" className="object-cover w-4" />
-      <span className="text-[#5b5b5b] flex flex-row gap-1">
-        <p>An Official Website of the</p>
-        <p className="font-semibold">Singapore Government</p>
-      </span>
+      <img
+        src={lionLogo}
+        alt="Logo of the Singapore Government"
+        className="object-cover w-4"
+      />
+      <div className="text-[#5b5b5b] flex flex-row gap-1 items-center">
+        <span>An Official Website of the</span>
+        <span className="font-semibold">Singapore Government</span>
+      </div>
     </div>
   );
 };
